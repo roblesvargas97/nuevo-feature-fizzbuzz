d4187d66289ce93e2d7544cebb8ef1c5ef572a55
roblesvargas97 <roblesvargas97@gmail.com>
2022-04-26 17:39:14 -0500

    Añadi mi test a la utileria reader

---
+++ test/lib/utils/Reader.test.js
@@ -0,0 +1,9 @@
+const ReaderClass = require("../../../lib/utils/Reader");
+
+describe("Testing Reader", () => {
+  test("Testinf function readJsonFile", () => {
+    expect(
+      ReaderClass.readJsonFile(process.cwd() + "/" + "explorers.json")
+    ).not.toBe("no such file or directory");
+  });
+});
