0bab26c11aa9c0a553c92203d4d53dec188fbe18
roblesvargas97 <roblesvargas97@gmail.com>
2022-04-27 16:34:38 -0500

    Añadi explorerInMission de acuerdo a los parametros y sus respectivas pruebas

--- test/lib/app.test.js
+++ test/lib/app.test.js
@@ -2,9 +2,26 @@ const request = require("supertest");
 const app = require("../../lib/app");
 
 describe("Test the root path ", () => {
-    test("should ", async (done) => {
+    test("response should be 200 ", async (done) => {
         const response = await request(app).get("/");
         expect(response.statusCode).toBe(200);
+        done();
+    });
+});
+
+describe("Explorer mission params", () => {
+    test("should ", async (done) => {
+        const response = await request(app).get("/v1/explorers/node");
+        expect(response.statusCode).toBe(200);
+        done();
+    });
+
+    test("should ", async (done) => {
+        const parameter = "node";
+        const response = await request(app).get("/v1/explorers/" + parameter);
+        response.body.forEach(element => {
+            expect(element.mission).toBe("node");
+        });
         done();
     });
 });
