02d2223ca56032e040cbee73e5d2073600a31b88
roblesvargas97 <roblesvargas97@gmail.com>
2022-04-26 22:31:29 -0500

    Reparando las inconsistencias eslint

--- test/lib/services/ExplorerService.test.js
+++ test/lib/services/ExplorerService.test.js
@@ -2,31 +2,31 @@ const ExplorerService = require("../../../lib/services/ExplorerService");
 const Reader = require("../../../lib/utils/Reader");
 
 describe("Test for ExplorerSeviceClass ", () => {
-  const explorers = Reader.readJsonFile(process.cwd() + "/" + "explorers.json");
+    const explorers = Reader.readJsonFile(process.cwd() + "/" + "explorers.json");
 
-  test("Explorers in Node ", () => {
-    ExplorerService.filterByMission(explorers, "node").forEach((element) => {
-      expect(element.mission).toBe("node");
+    test("Explorers in Node ", () => {
+        ExplorerService.filterByMission(explorers, "node").forEach((element) => {
+            expect(element.mission).toBe("node");
+        });
     });
-  });
 
-  test("Quantify of explorers in node mission", () => {
-    const numberOfExplorersInNode = ExplorerService.filterByMission(
-      explorers,
-      "node"
-    ).length;
-    expect(
-      ExplorerService.getAmountOfExplorersByMission(explorers, "node")
-    ).toEqual(numberOfExplorersInNode);
-  });
+    test("Quantify of explorers in node mission", () => {
+        const numberOfExplorersInNode = ExplorerService.filterByMission(
+            explorers,
+            "node"
+        ).length;
+        expect(
+            ExplorerService.getAmountOfExplorersByMission(explorers, "node")
+        ).toEqual(numberOfExplorersInNode);
+    });
 
-  test("Array by name of explorers in node", () => {
-    const numberOfExplorersInNode = ExplorerService.filterByMission(
-      explorers,
-      "node"
-    ).length;
-    expect(
-      ExplorerService.getExplorersUsernamesByMission(explorers, "node").length
-    ).toEqual(numberOfExplorersInNode);
-  });
+    test("Array by name of explorers in node", () => {
+        const numberOfExplorersInNode = ExplorerService.filterByMission(
+            explorers,
+            "node"
+        ).length;
+        expect(
+            ExplorerService.getExplorersUsernamesByMission(explorers, "node").length
+        ).toEqual(numberOfExplorersInNode);
+    });
 });
--- test/lib/services/FizzBuzzService.test.js
+++ test/lib/services/FizzBuzzService.test.js
@@ -3,34 +3,34 @@ const ExplorerService = require("../../../lib/services/ExplorerService");
 const Reader = require("../../../lib/utils/Reader");
 
 describe("Tests for FizzBuzzService", () => {
-  const explorers = Reader.readJsonFile(process.cwd() + "/" + "explorers.json");
-  const explorersInNode = ExplorerService.filterByMission(explorers, "node");
+    const explorers = Reader.readJsonFile(process.cwd() + "/" + "explorers.json");
+    const explorersInNode = ExplorerService.filterByMission(explorers, "node");
 
-  console.debug(explorersInNode);
+    console.debug(explorersInNode);
 
-  test(" Testing 4th explorer in node ", () => {
-    expect(
-      FizzBussService.applyValidationInExplorer(explorersInNode[3]).trick
-    ).toBe(FizzBussService.applyValidationInExplorer(explorersInNode[3]).score);
-  });
-  test(" Testing last explorer in node ", () => {
-    expect(
-      FizzBussService.applyValidationInExplorer(explorersInNode[9]).trick
-    ).toBe("FIZZBUZZ");
-  });
-  test(" Testing last explorer in node ", () => {
-    expect(
-      FizzBussService.applyValidationInExplorer(explorersInNode[9]).trick
-    ).toBe("FIZZBUZZ");
-  });
-  test(" Testing 2nd explorer in node ", () => {
-    expect(
-      FizzBussService.applyValidationInExplorer(explorersInNode[2]).trick
-    ).toBe("FIZZ");
-  });
-  test(" Testing 4th explorer in node ", () => {
-    expect(
-      FizzBussService.applyValidationInExplorer(explorersInNode[4]).trick
-    ).toBe("BUZZ");
-  });
+    test(" Testing 4th explorer in node ", () => {
+        expect(
+            FizzBussService.applyValidationInExplorer(explorersInNode[3]).trick
+        ).toBe(FizzBussService.applyValidationInExplorer(explorersInNode[3]).score);
+    });
+    test(" Testing last explorer in node ", () => {
+        expect(
+            FizzBussService.applyValidationInExplorer(explorersInNode[9]).trick
+        ).toBe("FIZZBUZZ");
+    });
+    test(" Testing last explorer in node ", () => {
+        expect(
+            FizzBussService.applyValidationInExplorer(explorersInNode[9]).trick
+        ).toBe("FIZZBUZZ");
+    });
+    test(" Testing 2nd explorer in node ", () => {
+        expect(
+            FizzBussService.applyValidationInExplorer(explorersInNode[2]).trick
+        ).toBe("FIZZ");
+    });
+    test(" Testing 4th explorer in node ", () => {
+        expect(
+            FizzBussService.applyValidationInExplorer(explorersInNode[4]).trick
+        ).toBe("BUZZ");
+    });
 });
