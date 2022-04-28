d801834c70b18469e8a15b2e77ae42399597f4b5
roblesvargas97 <roblesvargas97@gmail.com>
2022-04-28 18:10:47 -0500

    Añadi la funcion getNumberFizzBuzz con sus respectivas pruebas

--- test/lib/controllers/ExplorerController.test.js
+++ test/lib/controllers/ExplorerController.test.js
@@ -1,11 +1,13 @@
 const ExplorerController = require("../../../lib/controllers/ExplorerController");
 
 describe("Test for Explorer controller", () => {
+
     test("getExplorerByMission should return explorers in node", () => {
         ExplorerController.getExplorerByMission("node").forEach((explorer) =>
             expect(explorer.mission).toBe("node")
         );
     });
+
     test("getExplorerByMission should return explorers in java", () => {
         ExplorerController.getExplorerByMission("java").forEach((explorer) =>
             expect(explorer.mission).toBe("java")
@@ -58,4 +60,37 @@ describe("Test for Explorer controller", () => {
             expect(error.message).toBe("Explorers Not Found");
         }
     });
+
+    test("getNumberFizzBuzz FizzBuzz should return an error, when the value is not a number", () => {
+        try {
+            ExplorerController.getNumberFizzBuzz("Hola");
+        } catch (error) {
+            expect(error.message).toBe("The value should be a number");
+        }
+    });
+
+    test("getNumberFizzBuzz 15 should return FizzBuzz", () => {
+        expect(ExplorerController.getNumberFizzBuzz(15)).toBe("FIZZBUZZ");
+    });
+
+    test("getNumberFizzBuzz 10 should return Buzz", () => {
+        expect(ExplorerController.getNumberFizzBuzz(10)).toBe("BUZZ");
+    });
+
+    test("getNumberFizzBuzz 9 should return Fizz", () => {
+        expect(ExplorerController.getNumberFizzBuzz(9)).toBe("FIZZ");
+    });
+
+    test("getNumberFizzBuzz 7 should return 7", () => {
+        expect(ExplorerController.getNumberFizzBuzz(7)).toBe(7);
+    });
+
+    test("getNumberFizzBuzz 4.4 should return 4.4", () => {
+        expect(ExplorerController.getNumberFizzBuzz(4.4)).toBe(4.4);
+    });
+
+    test("getNumberFizzBuzz -9 should return Fizz", () => {
+        expect(ExplorerController.getNumberFizzBuzz(-9)).toBe("FIZZ");
+    });
+
 });
