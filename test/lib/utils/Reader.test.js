2292ded48f77e779419ed9ab18a53de78bed7a36
roblesvargas97 <roblesvargas97@gmail.com>
2022-04-28 23:16:55 -0500

    Agregando prueba al reader

--- test/lib/utils/Reader.test.js
+++ test/lib/utils/Reader.test.js
@@ -3,7 +3,7 @@ const ReaderClass = require("../../../lib/utils/Reader");
 describe("Testing Reader", () => {
     test("Testinf function readJsonFile", () => {
         expect(
-            ReaderClass.readJsonFile(process.cwd() + "/" + "explorers.json")
-        ).not.toBe("no such file or directory");
+            ReaderClass.readJsonFile(process.cwd() + "/test/" + "explorers.json")
+        ).not.toBeFalsy();
     });
 });
